66d9a4eca96291bea18479acb3d4ec79723e3600
CrazyDyz <yuanzhendai@gmail.com>
2015-11-20 19:31:27 +0800

    更正

--- Generate_Parentheses/Generate_Parentheses.py
+++ Generate_Parentheses/Generate_Parentheses.py
@@ -9,12 +9,17 @@ class Solution(object):
         list_before=self.generateParenthesis(n-1)
         list_now=[]
         for i in list_before:
-            if not i+'()' in list_now:
-                list_now.append(i+'()')
-            if not '()'+i in list_now:
-                list_now.append('()'+i)
-            if not '('+i+')' in list_now:
-                list_now.append('('+i+')')
+            i='(('+i
+            #print i
+            first=i.index(')')
+            for j in range(1,first):
+                #i[j]=')'
+                new=i[:j]+')'#+i[i.index(')')+1:]
+                #raw_input(i)
+                if j+1!=len(i):
+                    new+=i[j+1:]
+                if not new in list_now:
+                    list_now.append(new)
         return list_now
 
 print Solution().generateParenthesis(1)
