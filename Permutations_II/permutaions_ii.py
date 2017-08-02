22caef65008307ea11256ee257be44e156bd8251
yuanzhen.dai <yuanzhendai@gmail.com>
2017-08-02 21:45:00 +0800

    91.2% beat

--- Permutations_II/permutaions_ii.py
+++ Permutations_II/permutaions_ii.py
@@ -7,18 +7,12 @@
 # Date:	    2017-08-01 10:09:47
 #
 class Solution(object):
-    def permuteUnique(self, nums):
-        nums.sort()
-        return self.permuteSorted(nums)
-        
-    def permuteSorted(self, nums):
-        if len(nums) == 1:
-            return [nums]
-        ret = []
-        old_i = None
-        for i in xrange(len(nums)):
-            if nums[i] != old_i:
-                for j in self.permuteSorted(nums[:i] + nums[i+1:]):
-                    ret.append([nums[i]] + j)
-                old_i = nums[i]
-        return ret
+    def permute(self, nums):
+        result = [[]]
+        for i in nums:
+            new_result = []
+            for perm in result:
+                for j in xrange(len(perm) + 1):
+                    new_result.append(perm[:j] + [i] + perm[j:])
+            result = new_result
+        return result
