3d2ccd22fad06a26412dd10489cca357bf379794
yuanzhen.dai <yuanzhendai@gmail.com>
2017-08-05 17:10:20 +0800

    65% beat

--- N-Queens/N-Queens.py
+++ N-Queens/N-Queens.py
@@ -1,40 +1,30 @@
-import copy
 class Solution(object):
     def solveNQueens(self, n):
-        """
-        :type n: int
-        :rtype: List[List[str]]
-        """
-        init_case = [['x'] * n for _ in range(n)]
-        return [[''.join(j) for j in i] for i in self._solve([init_case], 0)]
-    
-    def _solve(self, cases, count):
-        if cases == [] or count >= len(cases[0]):
+        ret = []
+        for case in self._solve_by_3_list([[[-1] * n, [0] * n, [0] * n]], n, 0):
+            solution = ['' for _ in range(n)]
+            for i in range(n):
+                solution[case[0][i]] = '.' * i + 'Q' + '.' * (n - i - 1)
+            ret.append(solution)
+        return ret
+
+    def _solve_by_3_list(self, cases, div, n):
+        if cases == [] or div == n:
             return cases
         new_cases = []
-        for case in cases:
-            if not 'x' in case[count]:
-                continue
-            last = -1
-            for _ in xrange(case[count].count('x')):
-                last = case[count].index('x', last + 1)
-                new_cases.append(self._add(case, count, last))
-        return self._solve(new_cases, count+1)
-        
-    def _add(self, case, x, y):
-        new_case = copy.deepcopy(case)
-        new_case[x][y] = 'Q'
-        for i in xrange(len(new_case)):
-            if i != y:
-                new_case[x][i] = '.'
-            if i > x:
-                new_case[i][y] = '.'
-            if x + i < len(new_case) and y + i < len(new_case) and i != 0:
-                new_case[x+i][y+i] = '.'
-            if x + i < len(new_case) and y - i >= 0 and i != 0:
-                new_case[x+i][y-i] = '.'
-        return new_case
+        for row, ld, rd in cases:
+            ld = ld[1:] + [0]
+            rd = [0] + rd[:-1]
+            for i in range(div):
+                if row[i] != -1 or ld[i] != 0 or rd[i] != 0:
+                    continue
+                new_cases.append([
+                    row[:i] + [n] + row[i+1:],
+                    ld[:i] + [1] + ld[i+1:],
+                    rd[:i] + [1] + rd[i+1:],
+                ])
+        return self._solve_by_3_list(new_cases, div, n+1)
 
 
 if __name__ == '__main__':
-    print Solution().solveNQueens(2)
+    print Solution().solveNQueens(9)
