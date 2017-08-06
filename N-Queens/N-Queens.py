d14e5ace2cd7d1da4bc05c46d74062b244d5ac48
yuanzhen.dai <yuanzhendai@gmail.com>
2017-08-06 13:25:41 +0800

    Reduce the number of rows

--- N-Queens/N-Queens.py
+++ N-Queens/N-Queens.py
@@ -1,13 +1,10 @@
 class Solution(object):
     def solveNQueens(self, n):
-        ret = []
         queen_map = {1<<i: ('.' * i + 'Q' + '.' * (n - i - 1)) for i in range(n)}
-        ret = [[queen_map[i] for i in case[0]] for case in self._solve_by_3_list([[[], 0, 0, 0]], n)]
-        return ret
+        cases = [[[], 0, 0, 0]]
+        fil = (1 << n) - 1
 
-    def _solve_by_3_list(self, cases, div):
-        fil = (1 << div) - 1
-        for _ in range(div):
+        for _ in range(n):
             new_cases = []
             for record, row, ld, rd in cases:
                 ld = (ld << 1) & fil
@@ -16,15 +13,11 @@ class Solution(object):
                 while pos:
                     p = pos & (~pos + 1)
                     pos = pos - p
-                    new_cases.append([
-                        record + [p],
-                        row + p,
-                        ld + p,
-                        rd + p,
-                    ])
+                    new_cases.append([record + [p], row + p, ld + p, rd + p])
             cases = new_cases
-        return cases
+
+        return [[queen_map[i] for i in case[0]] for case in cases]
 
 
 if __name__ == '__main__':
-    print Solution().solveNQueens(11)
+    Solution().solveNQueens(11)
