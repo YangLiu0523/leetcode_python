6e2a7cbdd9b9349ce1c700e3b53d4e2e97327662
yuanzhen.dai <yuanzhendai@gmail.com>
2017-08-28 23:04:14 +0800

    Sqrt

---
+++ Sqrt/sqrt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# Title:    Sqrt
+# Author:   Dai Yuanzhen
+# Email:    yuanzhendai@gmail.com
+# Date:	    2017-08-28 23:04:04
+#
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(x ** 0.5)
