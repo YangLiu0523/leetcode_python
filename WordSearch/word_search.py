1ea5d66fe4f33471d1e79338bd1e3498aa737ce6
yuanzhen.dai <yuanzhendai@gmail.com>
2017-09-10 23:29:48 +0800

    dfs 99% beat

--- WordSearch/word_search.py
+++ WordSearch/word_search.py
@@ -6,28 +6,42 @@
 # Email:    yuanzhendai@gmail.com
 # Date:	    2017-09-10 23:23:26
 #
-class Solution(object):
+class Solution:
+    # @param board, a list of lists of 1 length string
+    # @param word, a string
+    # @return a boolean
     def exist(self, board, word):
-        ways = []
+        def dfs(x, y, word):
+            if len(word)==0: return True
+            #up
+            if x>0 and board[x-1][y]==word[0]:
+                tmp=board[x][y]; board[x][y]='#'
+                if dfs(x-1,y,word[1:]):
+                    return True
+                board[x][y]=tmp
+            #down
+            if x<len(board)-1 and board[x+1][y]==word[0]:
+                tmp=board[x][y]; board[x][y]='#'
+                if dfs(x+1,y,word[1:]):
+                    return True
+                board[x][y]=tmp
+            #left
+            if y>0 and board[x][y-1]==word[0]:
+                tmp=board[x][y]; board[x][y]='#'
+                if dfs(x,y-1,word[1:]):
+                    return True
+                board[x][y]=tmp
+            #right
+            if y<len(board[0])-1 and board[x][y+1]==word[0]:
+                tmp=board[x][y]; board[x][y]='#'
+                if dfs(x,y+1,word[1:]):
+                    return True
+                board[x][y]=tmp
+            return False
+
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    ways.append([[i, j]])
-        
-        for c in word[1:]:
-            new_ways = []
-            
-            for way in ways:
-                i, j = way[-1]
-                if i - 1 >= 0 and board[i-1][j] == c and [i-1, j] not in way:
-                    new_ways.append(way + [[i-1, j]])
-                if i + 1 < len(board) and board[i+1][j] == c and [i+1, j] not in way:
-                    new_ways.append(way + [[i+1, j]])
-                if j - 1 >= 0 and board[i][j-1] == c and [i, j-1] not in way:
-                    new_ways.append(way + [[i, j-1]])
-                if j + 1 < len(board[0]) and board[i][j+1] == c and [i, j+1] not in way:
-                    new_ways.append(way + [[i, j+1]])
-            ways = new_ways
-        
-        return not (ways == [])
-        
+                if board[i][j]==word[0]:
+                    if(dfs(i,j,word[1:])):
+                        return True
+        return False
