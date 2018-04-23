1f04ee6756b8847066a95c185f36308193273122
Lucas <yuanzhendai@gmail.com>
2018-04-23 21:33:42 +0800

    NB Plan: Day 10
    Question: 141
    Title: Linked List Cycle
    Difficulty: Easy
    Beat: 57.01%
    Date: 2018-04-19
    Comments:
    Very easy. Have seen this question before.

---
+++ 141_LinkedListCycle/solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# Title: 141. Linked List Cycle
+# Author: Lucas
+# Date: 2018-04-23 21:33:34
+#
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
+
