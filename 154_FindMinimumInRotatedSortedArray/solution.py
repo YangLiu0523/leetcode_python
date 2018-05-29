da800f2d652c73960b32c7ff071c5b38f4dfd497
Lucas <yuanzhendai@gmail.com>
2018-05-29 21:28:19 +0800

    NB Plan: Day 22
    Question: 154
    Title: Find Minimum in Rotated Sorted Array II
    Difficulty: Medium
    Beat: 95.10%
    Date: 2018-05-02
    Comments: Cheat on this one.

---
+++ 154_FindMinimumInRotatedSortedArray/solution.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Title: 154 Find Minimum in Rotated Sorted Array II
+# Author: Lucas
+# Date: 2018-05-29 21:27:58
+#
+class Solution:
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return min(nums)
+
