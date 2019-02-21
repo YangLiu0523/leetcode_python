3a1e5e3a50d0d26b7d3b812b02f568261a36e403
Lucas <yuanzhendai@gmail.com>
2019-02-21 21:49:59 +0800

    NB Plan: Day 66
    Question: 219
    Title: Contains Dupclicate III
    Difficulty: Medium
    Beat: 93%
    Date: 2019-02-20
    Comments:

---
+++ 220_ContainsDuplicateIII/solution.py
@@ -0,0 +1,26 @@
+# Copyright: Internal Use Only
+# Author: Lucas (yuanzhendai@gmail.com)
+# Date: 2019-02-19 21:24:29
+
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: 'List[int]', k: 'int', t: 'int') -> 'bool':
+        if t < 0:
+            return False
+
+        cache = {}
+        for index, num in enumerate(nums):
+            if index - k > 0:
+                bucket_id_remove = nums[index - k - 1] // (t + 1)
+                del cache[bucket_id_remove]
+
+            bucket_id = num // (t + 1)
+
+            if bucket_id in cache:
+                return True
+            if bucket_id - 1 in cache and abs(cache[bucket_id-1] - num) <= t:
+                return True
+            if bucket_id + 1 in cache and abs(cache[bucket_id+1] - num) <= t:
+                return True
+
+            cache[bucket_id] = nums[index]
+        return False
