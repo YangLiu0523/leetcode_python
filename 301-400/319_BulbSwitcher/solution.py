1600ffdb1621068b2f2047b173ca06150a223dae
Lucas Dai <yuanzhendai@gmail.com>
2019-05-17 08:15:52 +0800

    123 Since Plan
    Question: 319
    Title: Bulb Switcher
    Difficulty: Medium
    Beat: 98%
    Date: 2019-05-17
    Comments: Using math to calculate the answer.

---
+++ 301-400/319_BulbSwitcher/solution.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Author: Lucas
+# Date: 2019-05-17 08:15:38
+
+
+import math
+
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(math.sqrt(n))
