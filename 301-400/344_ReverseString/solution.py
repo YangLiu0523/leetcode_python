3c014568fb1e1d1ac0521bac9cc43f8ddbbfe005
Lucas Dai <yuanzhendai@gmail.com>
2019-05-28 20:57:27 +0800

    142 Since Plan
    Question: 344
    Title: Reverse String
    Difficulty: Easy
    Beat: 98%
    Date: 2019-05-28
    Comments:

---
+++ 301-400/344_ReverseString/solution.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Author: Lucas
+# Date: 2019-05-28 20:57:13
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+
